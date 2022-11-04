78710ca841274f551ce05e1896916c1b4cb5d87f
Daniel Zhekov <daniel.jekov60@gmail.com>
2022-11-04 13:49:34 +0200

    Removed the databse from the main Web project.

---
+++ AutoRepairCRM.Database/Data/AutoRepairCrmDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoRepairCRM.Database.Data;
+
+public class AutoRepairCrmDbContext : IdentityDbContext
+{
+    public AutoRepairCrmDbContext(DbContextOptions<AutoRepairCrmDbContext> options)
+    : base(options)
+    {
+        
+    }
+}
--- AutoRepairCRM/Program.cs
+++ AutoRepairCRM/Program.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using AutoRepairCRM.Data;
+using AutoRepairCRM.Database.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<AutoRepairCrmDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<AutoRepairCrmDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
